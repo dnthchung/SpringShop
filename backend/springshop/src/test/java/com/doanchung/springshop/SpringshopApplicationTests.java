5ce19e6b56b51beafe3ee38d26263037ffc4a2c4
chunChu <doanthanhchung03nb@gmail.com>
2024-03-14 17:37:44 +0700

    #9 BE setup
    - spring web
    - spring secu ( dua vao sau )
    - spring java jpa ( cầu nối db với service thông qua 1 cái reposi)
    - lombok ( dung anotation)
    - Validation

---
+++ backend/springshop/src/test/java/com/doanchung/springshop/SpringshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doanchung.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
